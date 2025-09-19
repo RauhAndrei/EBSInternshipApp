6dae3693ee3e9246a33bfb1f6ef5177e1ab78e6b
Andrei Rauh <rauhandrei69@gmail.com>
2025-09-19 20:54:36 +0300

    [Feature] Add ProfileView

---
+++ EBSInternshipApp/View/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  EBSInternshipApp
+//
+//  Created by Andrei Rauh on 19.09.2025.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Andrei")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
